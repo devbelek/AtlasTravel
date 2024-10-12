e8013f06abfa9ac6d30c421371d0e69f56605728
devbelek <belekasrarov10@gmail.com>
2024-10-12 22:55:16 +0600

    df

--- telegram_bot/utils.py
+++ telegram_bot/utils.py
@@ -1,12 +1,17 @@
+import asyncio
 from .bot import bot_application, send_notification
 from django.conf import settings
 
 
+async def async_send_notification(message):
+    await send_notification(bot_application.bot, settings.TELEGRAM_CHAT_ID, message)
+
+
 def send_consultation_notification(inquiry):
     message = f"Новый запрос на консультацию:\nИмя: {inquiry.name}\nТелефон: {inquiry.phone_number}\nEmail: {inquiry.email}\nСообщение: {inquiry.message}"
-    bot_application.bot.create_task(send_notification(bot_application.bot, settings.TELEGRAM_CHAT_ID, message))
+    asyncio.run(async_send_notification(message))
 
 
 def send_review_notification(review):
     message = f"Новый отзыв:\nИмя: {review.full_name}\nОценка: {review.rate}\nКомментарий: {review.text}"
-    bot_application.bot.create_task(send_notification(bot_application.bot, settings.TELEGRAM_CHAT_ID, message))
+    asyncio.run(async_send_notification(message))
