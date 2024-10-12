f3452c3d952a7ecc41812e67e2194890f9cbdcb7
devbelek <belekasrarov10@gmail.com>
2024-10-12 22:10:07 +0600

    dfdf

--- about/templatetags/safe_jazzmin_tags.py
+++ about/templatetags/safe_jazzmin_tags.py
@@ -13,3 +13,22 @@ def safe_sidebar_status(context):
         return jazzmin.sidebar_status(context)
     except AttributeError:
         return ""
+
+
+@register.simple_tag(takes_context=True)
+def safe_get_user_avatar(context):
+    request = context.get('request')
+    if not request or not hasattr(request, 'user'):
+        return ""
+    try:
+        return jazzmin.get_user_avatar({"user": request.user})
+    except AttributeError:
+        return ""
+
+
+@register.simple_tag(takes_context=True)
+def safe_get_menu(context):
+    try:
+        return jazzmin.get_menu(context)
+    except AttributeError:
+        return []
