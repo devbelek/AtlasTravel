bc92b7d2c63696b094eb2a29a023b8b8c46f10b6
devbelek <belekasrarov10@gmail.com>
2024-10-13 17:49:42 +0600

    df

--- common/serializers.py
+++ common/serializers.py
@@ -1,27 +1,32 @@
 from rest_framework import serializers
 from .models import Tag, City, Country, Comments, Inquiry
 
+
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
-        fields = ['id', 'name']
+        fields = '__all__'
+
 
 class CitySerializer(serializers.ModelSerializer):
     country = CountrySerializer(read_only=True)
 
     class Meta:
         model = City
-        fields = ['id', 'name', 'country']
+        fields = '__all__'
+
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ['id', 'name', 'icon']
+        fields = '__all__'
+
 
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
         fields = '__all__'
+
 
 class InquirySerializer(serializers.ModelSerializer):
     class Meta:
