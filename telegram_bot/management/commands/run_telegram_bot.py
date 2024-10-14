7baf7ee178a6fbb78ee020773cf754f9784df188
devbelek <belekasrarov10@gmail.com>
2024-10-14 06:32:39 +0600

    dfdf

--- telegram_bot/management/commands/run_telegram_bot.py
+++ telegram_bot/management/commands/run_telegram_bot.py
@@ -1,9 +1,14 @@
+import asyncio
 from django.core.management.base import BaseCommand
 from telegram_bot.bot import bot_application
+
 
 class Command(BaseCommand):
     help = 'Runs the Telegram bot'
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Starting Telegram bot...'))
-        bot_application.run_polling()
+
+        # Запуск асинхронного приложения
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(bot_application.start())
